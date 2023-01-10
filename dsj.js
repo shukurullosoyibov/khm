023b1f60b9150886f077236a83e7eee376040ced
shukurullosoyibov <shukurullosoyibov9@gmail.com>
2023-01-10 19:06:53 +0500

    khm-1

---
+++ dsj.js
@@ -0,0 +1,65 @@
+"use  strict";
+// let numbersOfSerials = prompt("nechta serial ko'rdingiz?");
+// confirm(numbersOfSerials);
+
+// let seriesDB = {
+//   count: numbersOfSerials,
+//   actor: [],
+//   privat: false,
+//   m: {},
+// };
+// confirm(seriesDB.privat);
+// confirm(seriesDB.count);
+// confirm(seriesDB.actor);
+// confirm(seriesDB.m);
+
+// let m = {
+//   messi: 5,
+//   lele: "sds",
+// };
+// m["men"] = `54`;
+// console.log(typeof m.men);
+// const age = +prompt("yoshingizni kiriting", "masalan 18");
+// if (age > 25) {
+//   console.log("salom siz ujis kino korishiz mumkin");
+// } else if (age > 18) {
+//   console.log("salom siz tarjima kino korishiz mumkin");
+// } else {
+//   console.log("salom siz multik korishiz mumkin");
+// }
+// let y;
+// for (let i = 1; i <= 14; i++) {
+//   y = prompt("rangiz", "sevimlisni kiriting");
+//   if (y == "qizil") {
+//     confirm(y);
+//     break;
+//   }
+// }
+
+// const fruits = ["Apple", "Banana", "Orange"];
+// for (x rt cal-al
+// ) {
+//   console.log(x);
+// }
+
+// let numbersOfSerials = prompt("nechta serial ko'rdingiz?");
+// confirm(numbersOfSerials);
+
+// for (i = 0; i < 2; i++) {
+//   prompt("Oxirgi ko’rgan serialingiz?");
+//   prompt("Nechi baxo berasiz?");
+// }
+// let a;
+
+// while (a != "oq") {
+//   a = prompt("rang");
+// }
+
+let a = prompt("ko’rgan seriallar sonin ");
+if (a > 10) {
+  alert("Siz serialchi zvezda ekansiz");
+} else if (a >= 5) {
+  alert("Siz classik tamoshabin ekansiz");
+} else {
+  alert("Kam serial koripsiz");
+}
